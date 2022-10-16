c4e6f6e8224fa42554ec0e5ce7f34e2e4cc7e013
Max Hernandez <maxhernandezcastillo@gmail.com>
2022-10-15 21:01:22 -0500

    Added example project

---
+++ example_project/src/components/Button.tsx
@@ -0,0 +1,15 @@
+import styles from 'src/styles/button.module.css';
+
+function Button() {
+  return (
+    <button 
+      type="button"
+      className={styles['button']}
+      onClick={() => console.log('Button clicked!')}
+    >
+      <h4 className="secondary-p">I am a Button!</h4>
+    </button>
+  );
+}
+
+export default Button;
